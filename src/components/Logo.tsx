c0eb796d29273949d730acb93566009e8890aedf
crownbugs <crownbugs@gmail.com>
2025-08-03 00:44:21 -0400

    Update Logo.tsx

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -12,7 +12,7 @@ const Logo: React.FC<LogoProps> = ({ showText = true, size = 'md', className }) 
   const sizes = {
     sm: 'h-8 w-8',
     md: 'h-10 w-10',
-    lg: 'h-14 w-14',
+    lg: 'h-20 w-20',
   };
   
   const textSizes = {
